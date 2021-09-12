00584f9cb3dd6e7ba4d2a9a6b3e01dc4af094b7c
Amir Hossein Kakavand <amirkkvnd99@gmail.com>
2021-09-12 11:27:46 +0430

    added website routes

--- seepa/src/routes/routes.tsx
+++ seepa/src/routes/routes.tsx
@@ -1,12 +1,34 @@
 import { Switch, Route } from "react-router-dom";
-import HomepageTopSection from "../components/layout/HomepageTopSection/HomepageTopSection.layout";
+import CategoryPage from "../components/pages/CategoryPage/CategoryPage.page";
+import Homepage from "../components/pages/Homapage/Homepage.page";
+import photoLinks from "../data/photos/photoLinks";
+import ProductPage from "../components/pages/ProductPage/ProductPage.page";
+import CartPage from "../components/pages/Cart/CartPage.page";
 
 
 const Routes: JSX.Element = <>
     <Switch>
         <Route path="/" exact>
-            <HomepageTopSection />
+            <Homepage />
         </Route>
+        <Route path="/cart">
+            <CartPage />
+        </Route>
+
+        {/* generate routes for each category page */}
+        {photoLinks.homepageCategories.map(category => {
+            return <Route path={"/" + category.name + "-shoes"}>
+                <CategoryPage Category={category.name} />
+            </Route>
+        })}
+
+        {Object.values(photoLinks.categories).map(category => {
+            return category.imageUrls.map(url => {
+                return <Route path={"/product-page/" + url}>
+                    <ProductPage imageSrc={url} />
+                </Route>
+            })
+        })}
     </Switch>
 </>
 
